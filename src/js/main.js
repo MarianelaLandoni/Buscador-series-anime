3031f1ff360f4a9c3e7ee3b118f38975f6abf792
Marianela Landoni Herrera <mlandonih@hotmail.com>
2022-07-18 09:17:26 +0200

    Add HTML elements in JS

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-console.log('>> Ready :)');
+//Traer elementos del HTML a JS con variables.
+
+const inputUser = document.querySelector('.js_inputUser');
+const btnSearch = document.querySelector('.js_btnSearch');
+const btnReset = document.querySelector('.js_btnReset');
+const listResults = document.querySelector('.js_listResults');
+const listFavorites = document.querySelector('.js_listFavorites');
